aba17459e689c7038a733ad89945433f17bf0779
sajib9988 <sajib.biswas9988@gmail.com>
2025-08-18 20:27:01 +0600

    ii

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from './../prisma/prisma.module';
 
 @Module({
-  providers: [UsersService, PrismaService],
+  imports: [PrismaModule],
+  providers: [UsersService],
   controllers: [UsersController],
   exports: [UsersService], 
 })

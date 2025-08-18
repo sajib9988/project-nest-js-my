d6b7186ea33b227d511ef8ca88eba84f249db0a7
sajib9988 <sajib.biswas9988@gmail.com>
2025-08-18 19:59:43 +0600

    y

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   async onModuleInit() {
